bc412fb4d994b870e3f9fa7229d8e289a0d463df
Y.A.S.1 <133089151+YAS-1@users.noreply.github.com>
2025-11-02 22:53:06 +0300

    getMessages route and controller

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,4 +1,5 @@
 import User from "../models/user.model.js";
+import Message from "../models/message.model.js";
 
 
 // Get users  for sidebar
@@ -14,4 +15,26 @@ export const getUsersForSidebar = async (req, res) => {
         res.status(500).json({ message: `Error getting users for sidebar: ${error}` });
     }
 };
+
+
+//  controller to get messages
+export const getMessages = async(req, res) => {
+    try {
+        const { id: userToChatId } = req.params
+        const myId = req.user._id
+
+        const messages = await Message.find({
+            $or:[
+                {senderId: myId, receiverId: userToChatId},
+                {senderId: userToChatId, receiverId: myId}
+            ]
+        })
+
+        res.status(200).json(messages)
+
+    } catch (error) {
+        console.log(`Error getting messages: ${error}`);
+        res.status(500).json({ message: `Error getting messages: ${error}` });
+    }
+}
 
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { protectRoute } from "../middleware/protectRoute.middleware.js";
-import { getUsersForSidebar } from "../controllers/message.controller.js";
+import { getUsersForSidebar, getMessages } from "../controllers/message.controller.js";
 
 
 const messageRoutes = express.Router();
 
 messageRoutes.get("/users", protectRoute, getUsersForSidebar)
+messageRoutes.get("/:id", protectRoute, getMessages)
 
 export default messageRoutes;
