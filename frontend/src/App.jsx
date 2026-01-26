6ecc3bc350b8a7f3e3871d77c9c5f69975a76977
Y.A.S.1 <133089151+YAS-1@users.noreply.github.com>
2026-01-26 10:48:14 +0300

    Implement routing and add page components for Home, Login, Sign Up, Profile, and Settings

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import Navbar from "./components/Navbar";
+import { Routes, Route} from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+import ProfilePage from "./pages/ProfilePage";
+import SettingsPage from "./pages/SettingsPage";
+
 
 const App = () => {
   return (
-    <div className="text-3xl font-bold underline text-red-400">
-      Tailwind is running
+    <div>
+
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+      </Routes>
+
     </div>
   )
 }
